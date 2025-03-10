f2b3397c90d6017bf6126d0fec6c4c38b1450765
Rahul0976 <rbrahman8076@gmail.com>
2025-03-10 22:43:03 +0530

    database init

--- src/app.js
+++ src/app.js
@@ -1,34 +1,31 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
-// admin Authentication
-const { authAdmin } = require("./midelware/auth");
-app.use("/admin", authAdmin);
-app.use("/admin/getdata", (req, res) => {
-  res.send("succesfull");
-});
-//intensnally creating error
-app.use("/admin/deletedata", (req) => {
-  res.send("succesfull delete");
-});
-app.use(
-  "/user",
-  (req, res, next) => {
-    //request Handler
-    console.log("first");
-    next();
-  },
-  (req, res) => {
-    console.log("second");
-    res.send("second res");
-  }
-);
-//Error Handle
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(404).send("Somthing geting wrong contact support team");
+const User = require("./models/user");
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "Rahul",
+    lastName: "jha",
+    emailId: "rahul@jha.com",
+    password: "rahul123",
+  };
+  // creating a new instance of the User model
+  const user = new User(userObj);
+  try {
+    await user.save();
+    res.send("user added succefully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
   }
 });
 
-app.listen(3000, () => {
-  console.log("server is on ");
-});
+connectDB()
+  .then(() => {
+    console.log("database connected");
+    app.listen(3000, () => {
+      console.log("server is on ");
+    });
+  })
+  .catch((err) => {
+    console.log("database not connected");
+  });
