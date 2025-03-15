329515553603d07aa6255569445d3ff6ec82b30b
Rahul0976 <rbrahman8076@gmail.com>
2025-03-15 17:41:19 +0530

    update in config folder

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://new_1:a3uc0GbIn85Gq7q4@cluster0.warhg.mongodb.net/"
+    "mongodb+srv://new_1:a3uc0GbIn85Gq7q4@cluster0.warhg.mongodb.net/devTinder"
   );
 };
 module.exports = connectDB;
